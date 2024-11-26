46fe7acb4eeed4b0ff4550df8d444cc8373bc3f3
yassineelmiri <yassireelhima@gmail.com>
2024-11-26 10:49:09 +0100

    feat(routes): add user routes for managing user-related endpoints

---
+++ backend/routes/usersRoute.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getAllUsersCtrl,
+  getUsersProfileCtrl,
+  updateUserProfileCtrl,
+  getUsersCountCtrl,
+  profilePhotoUploadCtrl,
+} = require("../controllers/userController");
+const {
+  verifyTokenAndAdmin,
+  verifyTokenAndOnlyUser,
+  VerifyToken,
+} = require("../middlewares/verifyToken");
+
+const validateObjectId = require("../middlewares/validateObjectId");
+const photoUpload = require("../middlewares/photoUpload");
+router.route("/profile").get( getAllUsersCtrl);
+router
+  .route("/profile/:id")
+  .get(validateObjectId, getUsersProfileCtrl)
+  .put(validateObjectId, verifyTokenAndOnlyUser, updateUserProfileCtrl);
+router
+  .route("/profile/profile-photo-upload")
+  .post(VerifyToken, photoUpload.single("image"), profilePhotoUploadCtrl);
+router.route("/count").get(verifyTokenAndAdmin, getUsersCountCtrl);
+
+module.exports = router;
